2586e8ace996093c133c5b92fe7838be5906bc6f
mariocrdev <87921814+mariocrdev@users.noreply.github.com>
2025-01-06 01:03:47 -0500

    confirm

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -102,7 +102,7 @@ const PatientForm = ({ onSuccess }: PatientFormProps) => {
                         </div>
                         <div className="space-y-2">
                             <Label htmlFor="sex">Sexo</Label>
-                            <div className="flex gap-4">
+                            <div className="grid gap-4">
                                 <div className="flex items-center gap-2">
                                     <input
                                         type="radio"
