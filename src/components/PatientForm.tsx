2361f377699cc7169a1d69079e139fa19b0550f0
mariocrdev <87921814+mariocrdev@users.noreply.github.com>
2025-01-06 01:01:13 -0500

    confirm

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -82,7 +82,7 @@ const PatientForm = ({ onSuccess }: PatientFormProps) => {
             <DialogHeader>
                 <DialogTitle>Agregar Nuevo Paciente</DialogTitle>
             </DialogHeader>
-            <ScrollArea className="h-[60vh] mt-4 pr-4">
+            <ScrollArea className="h-[60vh] w-[75vh] mt-4 pr-4">
                 <form ref={formRef} onSubmit={handleSubmit} className="space-y-4">
                     <div className="grid grid-cols-2 gap-4">
                         <div className="space-y-2">
