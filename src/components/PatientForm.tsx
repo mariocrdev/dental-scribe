c95d93d72136e9805ef5b2f73bb9a6e67b5c8782
mariocrdev <87921814+mariocrdev@users.noreply.github.com>
2025-01-07 21:05:53 -0500

    confirm

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -16,7 +16,7 @@ const patientSchema = z.object({
     first_name: z.string().min(2, { message: "El nombre debe tener al menos 2 caracteres." }),
     last_name: z.string().min(2, { message: "El apellido debe tener al menos 2 caracteres." }),
     birth_date: z.string().optional(),
-    phone: z.string().optional(),
+    phone: z.number({ message: "Numero inválido" }).positive().optional(),
     email: z.string().email({ message: "Correo electrónico inválido" }).optional(),
     address: z.string().optional(),
     medical_history: z.string().optional(),
