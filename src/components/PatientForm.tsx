25791191333679da6f626378831b170181933797
mariocrdev <87921814+mariocrdev@users.noreply.github.com>
2025-01-09 23:24:21 -0500

    c

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -180,7 +180,7 @@ const PatientForm = ({ onSuccess }: PatientFormProps) => {
     };
 
     return (
-        <div className="p-4 text-xl">
+        <div className="p-4 text-2xl">
             <DialogHeader>
                 <DialogTitle>Agregar Nuevo Paciente</DialogTitle>
             </DialogHeader>
